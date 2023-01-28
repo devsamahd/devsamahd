bfbd7b4982f7d1049d2e6a364810a24106540c32
devsamahd <devsamahd@gmail.com>
2023-01-28 20:55:20 +0100

    ...

--- components/About.tsx
+++ components/About.tsx
@@ -15,7 +15,7 @@ const About = () => {
               Hello! My name is Abdulsamad and I'm a software developer and a civil engineer(in training) from Nigeria <b>NG</b>. <br /> <br />
               I have over 3 years of experience, learning, exploring, teaching and leading engineering teams to build applications and solutions that span across several sectors.<br /> <br />
 
-              I'm always motivated to build or lead people to come up with what users will love and will satisfy business requirements. I am dynamic in my operations and I'm always yearning to make an impact with every line of code. <br /> <br />
+              I'm always motivated to build or lead people to come up with what users will love and will satisfy business requirements. I am dynamic in my operations and I'm always yearning to make an impact line by line. <br /> <br />
 
               Here are a few technologies I've been working with recently: <br /> <br />
               <Tags heading='' tags={["JavaScript(ES6+)","ReactJS/NextJS/Redux","HTML/(S)CSS","Figma","NodeJS/Express/MongoDB","UI/UX", "PHP/MySQL", "TypeScript"]} />
