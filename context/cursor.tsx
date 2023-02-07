9398b4e38032653545874b279bcd7973917abd0e
devsamahd <devsamahd@gmail.com>
2023-02-07 23:51:19 +0100

    ...

--- context/cursor.tsx
+++ context/cursor.tsx
@@ -3,15 +3,10 @@ const Cursor = () => {
 const { x, y } = useMousePosition();
   return (
     <>
-      <div
-        className={"ring"}
-        style={{ left: `${x}px`, top: `${y}px` }}
-      ></div>
-      
         <div className={"dot"}
         style={{ left: `${x}px`, top: `${y}px` }}
         color={'blue'}
-        />
+        > <img src="cursor.svg" alt="" /></div>
       
     </>
   );
