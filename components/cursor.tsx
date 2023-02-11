f083b657fadab30717eb544cb9242195e49b5d70
devsamahd <devsamahd@gmail.com>
2023-02-11 16:32:14 +0100

    ...

---
+++ components/cursor.tsx
@@ -0,0 +1,15 @@
+import { Box } from "@chakra-ui/react";
+import useMousePosition from "../context/useMousePos";
+const Cursor = () => {
+const { x, y } = useMousePosition();
+  return (
+    <Box display={{md:"inline", base:"none"}}>
+        <div className={"dot"}
+        style={{ left: `${x}px`, top: `${y}px` }}
+        color={'blue'}
+        > <img src="cursor.svg" alt="" /></div>
+      
+    </Box>
+  );
+};
+export default Cursor;
