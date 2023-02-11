83b18fd629580a7ffb0afc8c57f9fb60998e0d46
devsamahd <devsamahd@gmail.com>
2023-02-11 16:32:27 +0100

    added more meta tags

--- components/Head.tsx
+++ components/Head.tsx
@@ -1,11 +1,22 @@
 import Head from "next/head"
+import {useEffect} from 'react'
 
-const Meta = ({ title , desc }:{title:string, desc:string}) => {
+const Meta = ({ title , desc, image, type }:{title:string, desc:string, image:string, type:string}) => {
+  let getpath
+  useEffect(() => {
+    getpath = window.location.href
+  }, [])
   return (
     <Head>
         <title>{title}</title>
         <meta name="description" content={desc} />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <meta property="og:title" content={title} />
+        <meta property="og:site_name" content="Devsamahd" />
+        <meta property="og:url" content={getpath} />
+        <meta property="og:description" content={desc} />
+        <meta property="og:type" content={type} />
+        <meta property="og:image" content={image} />
         <link rel="icon" href="me.svg" />
         <link rel="shortcut icon" href="me.svg" type="image/x-icon" />
       </Head>
@@ -13,7 +24,9 @@ const Meta = ({ title , desc }:{title:string, desc:string}) => {
 }
 Meta.defaultProps = {
   title: "AO Abdulsalam | DevSamahd",
-  desc: "Abdulsalam Abdulsamad, professional software developer with a knack for creating effective and efficient software solutions."
+  desc: "Abdulsalam Abdulsamad, professional software developer with a knack for creating effective and efficient software solutions.",
+  image: "me.svg",
+  type: "website"
 }
 
 export default Meta
